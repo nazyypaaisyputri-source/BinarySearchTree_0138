42881f9e9f5245150259b25b0dbc4339462b350d
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-06-11 11:37:27 +0700

    bikin case 1 buat input kata dan panggil fungsi insert

--- binaryserach.cpp
+++ binaryserach.cpp
@@ -127,3 +127,11 @@ int main()
 
         switch (ch)
         {
+        case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            obj.insert(word);
+            break;
+        }
